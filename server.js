ba5f78b9c9dda12075a4eccf5314179d3bca3df4
Tyler Haas <haaser84@Tyler-Haass-MBP.hsd1.ut.comcast.net>
2015-11-30 23:17:17 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,26 @@
+(function(){
+  "use strict";
+
+  var express = require('express'),
+      routes  = require('./app/routes/index.js'),
+      mongo   = require('mongodb').MongoClient,
+      app     = express();
+
+  mongo.connect('mongodb://localhost:27017/clementinejs', function(err, db){
+    if(err) {
+      throw new Error("Database failed to connect!");
+    } else {
+      console.log("MongoDB successfully connected to port 27017.");
+    }
+
+    app.use('/public', express.static(process.cwd() + '/public'));
+    app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
+
+    routes(app,db);
+
+    app.listen(3000, function(){
+      console.log("listening on port 3000...");
+    });
+  });
+
+}());
