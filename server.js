ab0523d3c04b7225dc6c32d2873223d6a5ee493c
Tyler Haas <haasdevelopment@gmail.com>
2015-12-09 00:39:19 -0700

    created http server in node.js and created homepage view

--- server.js
+++ server.js
@@ -1,35 +1,12 @@
-(function(){
-  "use strict";
+'use strict';
+var express = require('express'),
+    routes  = require('./app/routes/index.js'),
+    app     = express();
 
-  var express  = require('express'),
-      routes   = require('./app/routes/index.js'),
-      mongoose = require('mongoose'),
-      passport = require('passport'),
-      session  = require('express-session'),
-      app      = express();
-  require('dotenv').load();
-  require('./app/config/passport')(passport);
+routes(app);
 
-  mongoose.connect(process.env.MONGO_URI);
-
-  app.use('/public', express.static(process.cwd() + '/public'));
-  app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
-  app.use('/common', express.static(process.cwd() + '/app/common'));
-
-  app.use(session({
-    secret: 'secretClementine',
-    resave: false,
-    saveUninitialized: true
-  }));
-
-  app.use(passport.initialize());
-  app.use(passport.session());
-
-  routes(app, passport);
-
-  var port = process.env.PORT || 3000;
-  app.listen(port, function(){
-    console.log("Node.js listening on port " + port + "...");
+app
+  .use('/public', express.static(process.cwd() + '/public'))
+  .listen(3000, function(){
+    console.log('Listening on port 3000...');
   });
-
-}());
