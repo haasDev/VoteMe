dea29c9ce6a1125e6a3cbd980e26db4b9568b539
Tyler Haas <haasdevelopment@gmail.com>
2015-12-09 22:56:34 -0700

    added home view

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 'use strict';
 var express = require('express'),
     routes  = require('./app/routes/index.js'),
+    mongo   = require('mongodb').MongoClient,
     app     = express();
 
-routes(app);
+mongo.connect('mongodb://localhost:27017/voteme', function(err, db){
+  if(err) throw new Error("Database failed to connect!")
+  else console.log("MongoDB successfully connected on port 27017");
 
-app
-  .use('/public', express.static(process.cwd() + '/public'))
-  .listen(3000, function(){
-    console.log('Listening on port 3000...');
-  });
+  routes(app, db);
+
+  app
+    .use('/public', express.static(process.cwd() + '/public'))
+    .use('/controllers', express.static(process.cwd() + '/app/controllers'))
+    .listen(3000, function(){
+      console.log('Listening on port 3000...');
+    });
+});
